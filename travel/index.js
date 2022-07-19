747b1c8c75387e05ccdfc9aacca0f5022bc29e93
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-07-19 12:40:04 +0300

    feat: add pop-up

--- travel/index.js
+++ travel/index.js
@@ -1,202 +1,70 @@
-// BURGER MENU
-const menu = document.querySelector('.menu');
-const overlay = document.querySelector('.overlay');
-const burger = document.querySelector('.burger');
-const burgerClose = document.querySelector('.burger-close');
-const navList = document.querySelector('.nav-list');
-const active = function() {
-  menu.classList.toggle('active');
-  overlay.classList.toggle('active');
-  document.body.classList.toggle('lock');
+// POP-UP
+const register = () => {                                                                          //change .pop-up to register
+  document.querySelector('.pop-up__title-text').innerHTML = 'Create account';                     //change title text
+  document.querySelector('.facebook').classList.add('register');                                  //remove facebook button
+  document.querySelector('.google').classList.add('register');                                    //remove google button
+  document.querySelector('.or').classList.add('register');                                        //remove or div
+  document.querySelector('.forgot').classList.add('register');                                    //remove forgot text
+  document.querySelector('.sign').innerHTML = 'Sign Up';                                          //change sign button text
+  document.querySelector('.bottom-text').innerHTML = 'Already have an account? <span class="last-text-login">Log in</span>';    //change bottom text
 };
-burger.addEventListener("click", active);
-burgerClose.addEventListener("click", active);
-navList.addEventListener("click", active);
-overlay.addEventListener("click", active);
+const login = () => {                                                                             //change .pop-up to login
+  document.querySelector('.pop-up__title-text').innerHTML = 'Log in to your account';             //change title text
+  document.querySelector('.facebook').classList.remove('register');                               //add facebook button
+  document.querySelector('.google').classList.remove('register');                                 //add google button
+  document.querySelector('.or').classList.remove('register');                                     //add or div
+  document.querySelector('.forgot').classList.remove('register');                                 //add forgot text
+  document.querySelector('.sign').innerHTML = 'Sign In';                                          //change sign button text
+  document.querySelector('.bottom-text').innerHTML = 'Don’t have an account? <span class="last-text-register">Register</span>';    //change bottom text
+};
+document.querySelector('.button-login').addEventListener("click", () => {                         //listen click on .button-login
+  document.querySelector('.pop-up').classList.add('slideout');                                    //open .pop-up
+  document.querySelector('.overlay').classList.add('active');                                     //make .overlay visible
+  document.body.classList.add('lock');                                                            //lock scroll
+});
+document.querySelector('.pop-up').addEventListener("click", (event) => {                          //listen click on .pop-up
+  if (event.target.className === 'last-text-register') {register()                                //change .pop-up to login by click on .last-text-register
+} else if (event.target.className === 'last-text-login') {login()}});                             //change .pop-up to register by click on .last-text-login
+// BURGER MENU
+const openMenu = () => {
+  document.querySelector('.menu').classList.add('active');                                        //open .menu
+  document.querySelector('.overlay').classList.add('active');                                     //make .overlay visible
+  document.body.classList.add('lock');};                                                          //lock scroll
+const closeMenu = () => {
+  document.querySelector('.menu').classList.remove('active');                                     //close .menu
+  document.querySelector('.overlay').classList.remove('active');                                  //make .overlay unvisible
+  document.body.classList.remove('lock');                                                         //unlock scroll
+  document.querySelector('.pop-up').classList.remove('slideout');                                 //close .pop-up
+  document.querySelector('.pop-up').ontransitionend = () => {login()}};                           //change .pop-up to login
+document.querySelector('.burger').addEventListener("click", ()=>{openMenu()});                    //open .menu by click on .burger
+document.querySelector('.burger-close').addEventListener("click", ()=>{closeMenu()});             //close .menu by click on .burger-close
+document.querySelector('.nav-list').addEventListener("click", (event)=>{
+  if (event.target.className !== 'nav-link nav-login hide') {closeMenu()}                         //close .menu by click on .nav-list
+  else {document.querySelector('.menu').classList.remove('active');                               //close .menu by click on .nav-list
+        document.querySelector('.pop-up').classList.add('slideout');}});                          //open .pop-up by click on .nav-login
+document.querySelector('.overlay').addEventListener("click", ()=>{closeMenu()});                  //close .menu by click on .overlay
 // SLIDER
-const carousel = document.querySelector('.carousel');
-const spain = document.querySelector('.spain');
-const spain2 = document.querySelector('.spain2');
-const japan = document.querySelector('.japan');
-const japan2 = document.querySelector('.japan2');
-const usa = document.querySelector('.usa');
-const usa2 = document.querySelector('.usa2');
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
-const leftClick = document.querySelector('.left-click');
-const rightClick = document.querySelector('.right-click');
-const filled = document.querySelector('.filled');
-let orderDesktop = 0;
-let orderMobile = -1;
-leftClick.addEventListener("click", function() {
-  if (orderDesktop === 0) {
-    orderDesktop = -1;
-    filled.style = "order: -1";
-    carousel.classList.toggle('left');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('left');
-      spain2.style = "order: 1";
-      japan2.style = "order: 2";
-      usa2.style = "order: 3";
-      spain.style = "order: 4";
-      japan.style = "order: 5";
-      usa.style = "order: 6";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderDesktop === -1) {
-    orderDesktop = 1;
-    filled.style = "order: 1";
-    carousel.classList.toggle('left');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('left');
-      spain2.style = "order: 2";
-      japan2.style = "order: 3";
-      usa2.style = "order: 4";
-      spain.style = "order: 5";
-      japan.style = "order: 6";
-      usa.style = "order: 1";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderDesktop === 1) {
-    orderDesktop = 0;
-    filled.style = "order: 0";
-    carousel.classList.toggle('left');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('left');
-      spain2.style = "order: 3";
-      japan2.style = "order: 4";
-      usa2.style = "order: 5";
-      spain.style = "order: 6";
-      japan.style = "order: 1";
-      usa.style = "order: 2";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-}})
-rightClick.addEventListener("click", function() {
-  if (orderDesktop === 0) {
-    orderDesktop = 1;
-    filled.style = "order: 1";
-    carousel.classList.toggle('right');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('right');
-      spain2.style = "order: 5";
-      japan2.style = "order: 6";
-      usa2.style = "order: 1";
-      spain.style = "order: 2";
-      japan.style = "order: 3";
-      usa.style = "order: 4";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderDesktop === 1) {
-    orderDesktop = -1;
-    filled.style = "order: -1";
-    carousel.classList.toggle('right');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('right');
-      spain2.style = "order: 4";
-      japan2.style = "order: 5";
-      usa2.style = "order: 6";
-      spain.style = "order: 1";
-      japan.style = "order: 2";
-      usa.style = "order: 3";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderDesktop === -1) {
-    orderDesktop = 0;
-    filled.style = "order: 0";
-    carousel.classList.toggle('right');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('right');
-      spain2.style = "order: 3";
-      japan2.style = "order: 4";
-      usa2.style = "order: 5";
-      spain.style = "order: 6";
-      japan.style = "order: 1";
-      usa.style = "order: 2";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-}})
-leftArrow.addEventListener("click", function() {
-  if (orderMobile === 0) {
-    orderMobile = -1;
-    filled.style = "order: -1";
-    carousel.classList.toggle('left');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('left');
-      spain.style = "order: 2";
-      japan.style = "order: 3";
-      usa.style = "order: 1";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderMobile === -1) {
-    orderMobile = 1;
-    filled.style = "order: 1";
-    carousel.classList.toggle('left');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('left');
-      spain.style = "order: 3";
-      japan.style = "order: 1";
-      usa.style = "order: 2";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderMobile === 1) {
-    orderMobile = 0;
-    filled.style = "order: 0";
-    carousel.classList.toggle('left');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('left');
-      spain.style = "order: 1";
-      japan.style = "order: 2";
-      usa.style = "order: 3";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-}})
-rightArrow.addEventListener("click", function() {
-  if (orderMobile === 0) {
-    orderMobile = 1;
-    filled.style = "order: 1";
-    carousel.classList.toggle('right');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('right');
-      spain.style = "order: 3";
-      japan.style = "order: 1";
-      usa.style = "order: 2";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderMobile === 1) {
-    orderMobile = -1;
-    filled.style = "order: -1";
-    carousel.classList.toggle('right');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('right');
-      spain.style = "order: 2";
-      japan.style = "order: 3";
-      usa.style = "order: 1";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-  } else if (orderMobile === -1) {
-    orderMobile = 0;
-    filled.style = "order: 0";
-    carousel.classList.toggle('right');
-    setTimeout(function() {
-      carousel.style = "transition: all 0s ease 0s";
-      carousel.classList.toggle('right');
-      spain.style = "order: 1";
-      japan.style = "order: 2";
-      usa.style = "order: 3";
-    }, 1000);
-    carousel.style = "transition: all 1s ease 0s";
-}})
+let order = 0;                                                                                    //create var order
+const setOrder = () => document.querySelector(':root').style.setProperty('--order', order);       //set css var(--order) = order
+const leftDots = () => {(order === -1) ? (order = 1, setOrder()) : (order -= 1, setOrder())};     //set scroller order by .left-click
+const rightDots = () => {(order === 1) ? (order = -1, setOrder()) : (order += 1, setOrder())};    //set scroller order by .right-click
+const pictures = [];                                                                              //create arr pictures
+document.querySelectorAll('.picture').forEach((_, index, array) => {pictures[index] = array[index].innerHTML});   //fill arr pictures
+pictures.push(pictures[Math.floor(pictures.length/2)]);                                           //add middle element to the end
+const changeOrder = () => document.querySelectorAll('.picture').forEach((_, index, array) => {array[index].innerHTML = pictures[index]});   //change order of pictures in html
+const left = () =>  {pictures.unshift(pictures.pop()); changeOrder(); leftDots();}                //change order in arr pictures by .left-click
+const right = () => {pictures.push(pictures.shift()); changeOrder(); rightDots();}                //change order in arr pictures by .right-click
+const offset = document.querySelectorAll('.picture')[1].offsetLeft - document.querySelectorAll('.picture')[0].offsetLeft.toString()+"px";   //calc offset
+document.querySelector(':root').style.setProperty('--offset', offset);                            //set css var(--offset) = offset
+const carousel = document.querySelector('.carousel');                                             //create var carousel
+if (document.body.clientWidth <= 390) {left();}                                                   //move left when .body width <= 390px
+document.querySelector('.left-click').addEventListener("click", () => {
+  carousel.classList.add('left'); carousel.ontransitionend = () => {carousel.classList.remove('left'); left()}});       //move left by .left-click
+document.querySelector('.right-click').addEventListener("click", () => {
+  carousel.classList.add('right'); carousel.ontransitionend = () => {carousel.classList.remove('right'); right()}});    //move right by .right-click
+document.querySelector('.left-arrow').addEventListener("click", () => {
+  carousel.classList.add('left'); carousel.ontransitionend = () => {carousel.classList.remove('left'); left()}});       //move left by .left-arrow
+document.querySelector('.right-arrow').addEventListener("click", () => {
+  carousel.classList.add('right'); carousel.ontransitionend = () => {carousel.classList.remove('right'); right()}});    //move right by .right-arrow
 // CONSOLE
-console.log('1. Вёрстка соответствует макету. Ширина экрана 390px +48\n2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки.\nВесь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n3. На ширине экрана 390рх и меньше реализовано адаптивное меню +22\n\nИтого 75 баллов')
+// console.log('1. Вёрстка соответствует макету. Ширина экрана 390px +48\n2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки.\nВесь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n3. На ширине экрана 390рх и меньше реализовано адаптивное меню +22\n\nИтого 75 баллов')
